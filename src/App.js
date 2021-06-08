b0d0b47797cbf29ab87b283b96c2b0465102f5aa
Eshwar Nag Pilli <pillieshwar@gmail.com>
2021-06-08 09:29:40 -0700

    added header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from "./logo.svg";
+import "./App.css";
+// import Frontpage from "./dashboard/Frontpage";
+import Header from "./dashboard/Header";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header />
+      {/* <Frontpage /> */}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +20,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
